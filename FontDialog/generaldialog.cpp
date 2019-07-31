6c89d5e32829700287e0b1b8dc3cbcdba7f1e809
Ashish Singh <ashish.singh@uphs.upenn.edu>
2019-07-31 10:06:41 -0400

    add style sheet

--- FontDialog/generaldialog.cpp
+++ FontDialog/generaldialog.cpp
@@ -1,14 +1,39 @@
 #include "generaldialog.h"
 #include "ui_generaldialog.h"
+#include <QDebug>
+#include <QFile>
 
 GeneralDialog::GeneralDialog(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::GeneralDialog)
 {
     ui->setupUi(this);
+
+    connect(ui->comboBox,SIGNAL(currentIndexChanged(int)),this,SLOT(OnChangeTheme(int)));
 }
 
 GeneralDialog::~GeneralDialog()
 {
     delete ui;
+}
+
+void GeneralDialog::OnChangeTheme(int theme)
+{
+    ThemeType t = ThemeType(theme);
+    qDebug() << t << endl;
+    if(t == ThemeType::Dark)
+    {
+        qDebug() << "dark theme " << endl;
+        QFile qss("C:\\workspace\\QtTests\\build-FontDialog-Desktop_Qt_5_11_1_MSVC2017_64bit-Debug\\debug\\darkTheme.qss");
+        bool success = qss.open(QFile::ReadOnly);
+        qDebug() << "success = " << success << endl;
+        if(success)
+        {
+            qDebug() << "reading succeeded" << endl;
+            qApp->setStyleSheet(qss.readAll());
+            qss.close();
+        }
+    }
+    else
+        qApp->setStyleSheet("");
 }
--- FontDialog/generaldialog.h
+++ FontDialog/generaldialog.h
@@ -2,6 +2,7 @@
 #define GENERALDIALOG_H
 
 #include <QWidget>
+#include <QObject>
 
 namespace Ui {
 class GeneralDialog;
@@ -12,8 +13,15 @@ class GeneralDialog : public QWidget
     Q_OBJECT
 
 public:
+    enum ThemeType{Light,Dark};
+
     explicit GeneralDialog(QWidget *parent = nullptr);
     ~GeneralDialog();
+
+    Q_ENUM(ThemeType)
+
+public slots:
+    void OnChangeTheme(int);
 
 private:
     Ui::GeneralDialog *ui;
