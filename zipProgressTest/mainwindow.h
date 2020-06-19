0e6faba07d67a8ca28ab1d77969a50b1dc9332c1
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-06-19 15:03:44 -0400

    extraction with progress update

---
+++ zipProgressTest/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void OnInputButtonClicked();
+    void OnOutputDirectoryButtonClicked();
+    void OnRunButtonClicked();
+    void SetProgress(int);
+
+private:
+    Ui::MainWindow *ui;
+    QString mInputZipFile, mOutputDirecty;
+};
+
+#endif // MAINWINDOW_H
