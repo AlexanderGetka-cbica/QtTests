eb17ec96f1d4444c44d6fcf6dc5d5e0a4bd19924
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-07-06 19:50:10 -0400

    remove extra cout msgs

--- DownloadExtractCLITest/main.cpp
+++ DownloadExtractCLITest/main.cpp
@@ -6,19 +6,31 @@
 #include "progressupdater.h"
 #include <QObject>
 #include <QTimer>
+#include <iostream>
+
+void UpdateProgress(int progress)
+{
+    std::cout << " progress = " << progress << "\t\r" << std::flush;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //QString mDownloadLink = "ftp://www.nitrc.org/home/groups/captk/downloads/SampleData_1.6.0/PseudoProgressionEstimator.zip";
-    QString mDownloadLink = "ftp://www.nitrc.org/home/groups/captk/downloads/libra.zip";
-    QString mDownloadFolder = "C:/workspace/QtTests/DownloadExtractCLITest/";
+//    std::cout << " progress = " << 0 << std::endl;/*<< "\t\r" << std::flush;*/
+
+//    std::cout << " hello world " << std::endl;
+
+    QString mDownloadLink = "ftp://www.nitrc.org/home/groups/captk/downloads/SampleData_1.6.0/PseudoProgressionEstimator.zip";
+    //QString mDownloadLink = "ftp://www.nitrc.org/home/groups/captk/downloads/libra.zip";
+    QString mDownloadFolder = "C:/workspace/Qt/downloadextractCLItestOut/";
 
     ApplicationDownloadManager *adm = new ApplicationDownloadManager();
+    QObject::connect(adm,&ApplicationDownloadManager::updateProgressSignal,UpdateProgress);
     adm->SetDownloadLink(mDownloadLink);
     adm->SetDownloadFolder(mDownloadFolder);
     adm->getApplication("libra", true);
+    //QTimer::singleShot(0,adm,SLOT(getApplication("libra", true)));
 
 //    ThreadedDownload *td = new ThreadedDownload();
 //    td->SetDownloadLink(mDownloadLink);
@@ -27,5 +39,6 @@ int main(int argc, char *argv[])
 
 //    QTimer::singleShot(0,td,SLOT(start()));
     return a.exec();
+    //return 0;
 }
 
